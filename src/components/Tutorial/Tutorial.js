c78fcb6a83083c21c47fa621c69cd6889e07c52b
Pat Rivet <patrickmrivet@gmail.com>
2021-04-01 11:42:28 +0100

    Add display of tutorial - very basic layout and styling.

--- src/components/Tutorial/Tutorial.js
+++ src/components/Tutorial/Tutorial.js
@@ -1,5 +1,28 @@
+import "./Tutorial.css";
+
 const Tutorial = ({ tutorial }) => {
-  return <div>tutorial</div>;
+  const ratingPercentageRounded = Math.round(tutorial.averageUserRating * 100);
+
+  return (
+    <div className="tutorial">
+      <p className="tutorial__title">
+        <strong>Title: </strong>
+        {tutorial.videoTitle}
+      </p>
+      <p className="tutorial__title">
+        <strong>Teacher: </strong>
+        {tutorial.teacherName}
+      </p>
+      <p className="tutorial__title">
+        <strong>Average User Rating: </strong>
+        {ratingPercentageRounded}%
+      </p>
+      <p className="tutorial__tags">
+        <strong>Tags: </strong>
+        {tutorial.tags.toString()}
+      </p>
+    </div>
+  );
 };
 
 export default Tutorial;
