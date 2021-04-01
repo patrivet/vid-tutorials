165ba6452824f3ec9a1949b2453547c6df7e4df2
Pat Rivet <patrickmrivet@gmail.com>
2021-04-01 12:01:59 +0100

    (Refactor) Move API fetch to App.js. Add react context. Add refresh tutorials button.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
+import { useState, useEffect, createContext, useContext } from "react";
 import "./App.css";
+
+// Custom components
+import * as apiService from "./services/apiService";
+
 import TutorialsView from "./components/TutorialsView";
 
+// App Context setup
+const AppContext = createContext();
+export const useAppContext = () => useContext(AppContext);
+
 function App() {
+  const [tutorials, setTutorials] = useState([]);
+  const [ready, setReady] = useState(false);
+
+  const fetchTutorials = () => {
+    apiService
+      .getTutorials()
+      .then((tutorials) => {
+        setTutorials(tutorials);
+      })
+      .then(() => setReady(true));
+  };
+
+  // Call API on component mount
+  useEffect(() => {
+    fetchTutorials();
+  }, []);
+
+  // Define context
+  const context = {
+    tutorials,
+    setTutorials,
+    fetchTutorials,
+  };
+
   return (
-    <div className="App">
-      <TutorialsView />
-    </div>
+    <AppContext.Provider value={context}>
+      <div className="App">
+        {ready ? <TutorialsView /> : <div>Loading...</div>}
+      </div>
+    </AppContext.Provider>
   );
 }
 
